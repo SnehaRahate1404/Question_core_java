916d61a221ccfd0069e5aec4911ca8948666bc61
Sneha_Rahate <sneharahateytl1404@gmail.com>
2024-06-29 11:16:50 +0530

    merge sort andquick sort addex

--- sorting_algorithms.java
+++ sorting_algorithms.java
@@ -13,7 +13,73 @@ public class sorting_algorithms {
             }
          }
     }
+    // merge sort
+    //tc = O(nlogn)
+    public static void mergesort(int arr[] , int si, int ei){
+        if(si>=ei){
+            return;  //base case
+        }
+        int mid = si +(ei-si)/2;
+        mergesort(arr, si, mid); //left
+        mergesort(arr, mid+1, ei); // right
+        merge(arr,si,mid,ei);
+    }
+    public static void merge(int arr[], int si , int mid, int ei){
+        int temp [] = new int[ei-si+1];
+        int i =si;
+        int j =mid+1;
+        int k =0;
+        while(i<=mid && j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }
+            else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
 
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+        while(j<=ei){
+            temp[k++] = arr[j++];
+        }
+
+        for(k = 0, i=si;k<temp.length;k++,i++){
+            arr[i] = temp[k];
+        }
+    }
+    //quick sort
+    public static void quickSort(int arr[] , int si , int ei){
+        if(si>=ei){
+            return;
+        }
+
+        int pIndx = partition(arr,si,ei);
+        quickSort(arr, si, pIndx-1);
+        quickSort(arr,pIndx+1,ei);
+    }
+    public static int partition(int arr[] , int si,int ei){
+        int pivot = arr[ei];
+        int i = si-1; //make space for elements smaller than the pivot to get stored
+        for(int j=si;j<ei ;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }  
+        }
+         // for pivot
+         i++;
+         int temp = pivot;
+         arr[ei] = arr[i];
+         arr[i] = temp;
+         return i;
+    }
     //selection sort
     public static void selection(int array[]){
         for(int i =0;i< array.length - 1 ;i++){
@@ -35,8 +101,9 @@ public class sorting_algorithms {
         }
     }
     public static void main(String args[]){
-        int array[] = {5,4,1,3,2};
-        selection(array);
+        int array[] = {5,4,1,3,2,-1};
+        //selection(array);
+        quickSort(array, 0, array.length-1);
         display(array);
     }
 }
